8662f6a6bc486df14e53dfe934e1909cca0acf58
kyamazawa <yamazawa@supersoftware.co.jp>
2018-06-04 01:41:00 +0900

    update main

--- main.go
+++ main.go
@@ -3,18 +3,18 @@ package main
 import (
 	"log"
 
-	"github.com/kyamazawa/glue-go/components/awesome"
 	"github.com/kyamazawa/glue-go/provider"
-	"github.com/kyamazawa/glue-go/service"
 )
 
 func main() {
+	// awesomePresenter := awesome.NewPresenter()
+	// awesomeInteractor := awesome.NewInteractor(awesome.WithPresenter(awesomePresenter))
+	// awesomeController := awesome.NewController(awesome.WithInteractor(awesomeInteractor))
+	// awesomeService := service.NewAwesomeServer(service.WithAwesomeSDK(awesomeController))
+	// awesomeProvider := provider.NewAwesome(provider.WithAwesomeService(awesomeService))
+	awesomeProvider := provider.NewAwesome()
+
 	ch := make(chan bool)
-	awesomePresenter := awesome.NewPresenter()
-	awesomeInteractor := awesome.NewInteractor(awesome.WithPresenter(awesomePresenter))
-	awesomeController := awesome.NewController(awesome.WithInteractor(awesomeInteractor))
-	awesomeService := service.NewAwesomeServer(service.WithAwesomeSDK(awesomeController))
-	awesomeProvider := provider.NewAwesome(provider.WithAwesomeService(awesomeService))
 	go awesomeProvider.Run()
 	log.Println(<-ch)
 }

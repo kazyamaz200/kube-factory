c7510b5671f874c372b10606466b1d90bb804640
kyamazawa <yamazawa@supersoftware.co.jp>
2018-06-22 00:55:18 +0900

    add basic endpoint

--- service/server/factory_http.go
+++ service/server/factory_http.go
@@ -57,21 +57,58 @@ func (s *FactoryHTTP) Start() net.Listener {
 }
 
 func (s *FactoryHTTP) config() {
-	s.router.HandleFunc("/api/clusters", s.postClusters).Methods("POST")
+	api := s.router.PathPrefix("/api").Subrouter()
+	api.Use(jsonResponseMiddleware)
+	api.HandleFunc("/clusters", s.postClusters).Methods("POST")
+	api.HandleFunc("/clusters", s.getClusters).Methods("GET")
+	api.HandleFunc("/clusters/{id}", s.getClustersID).Methods("GET")
+	api.HandleFunc("/clusters/{id}", s.putClustersID).Methods("PUT")
+	api.HandleFunc("/clusters/{id}", s.deleteClustersID).Methods("DELETE")
 }
 
+func jsonResponseMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		next.ServeHTTP(w, r)
+	})
+}
+
+// POST /api/clusters
 func (s *FactoryHTTP) postClusters(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-
 	result, err := s.sdk.CreateCluster()
-
 	var body []byte
 	if err != nil {
-		w.WriteHeader(400)
+		w.WriteHeader(http.StatusBadRequest)
 		body, _ = json.Marshal(err)
 	} else {
+		w.WriteHeader(http.StatusAccepted)
 		body, _ = json.Marshal(result)
 	}
-
 	w.Write(body)
+}
+
+// GET /api/clusters
+func (s *FactoryHTTP) getClusters(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+}
+
+// GET /api/clusters/{id}
+func (s *FactoryHTTP) getClustersID(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Cluster: %v\n", vars["id"])
+}
+
+// PUT /api/clusters/{id}
+func (s *FactoryHTTP) putClustersID(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Cluster: %v\n", vars["id"])
+}
+
+// DELETE /api/clusters/{id}
+func (s *FactoryHTTP) deleteClustersID(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Cluster: %v\n", vars["id"])
 }
